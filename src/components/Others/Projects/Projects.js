f45c1cce101d48064091f2927909ecd545768cf6
Quentin Carlin <quentincarlin@Quentins-MacBook-Air.local>
2018-12-19 10:56:43 -0500

    i think its messing up my other project

--- src/components/Others/Projects/Projects.js
+++ src/components/Others/Projects/Projects.js
@@ -14,12 +14,12 @@ class Projects extends Component {
             <div class='cover'>
                 <h1>My Projects</h1>
                     <div className='grid2'>
-                        <div className='grid-item'><a target='blank' href='https://codepen.io/QuentinCarlin/pen/aaNgEL'> CSS Creature<img src={img2}/></a>   Built after 1 day of coding <br/> Time to build: 1 day </div>
-                        <div className='grid-item'><a target='blank' href='https://carlinquentin.github.io/firstWebsite/'> Static Store Front<img src={img3}/></a>  Built after 5 days of coding <br/> Time to build: 3 days </div>
-                        <div className='grid-item'><a target='blank' href='https://carlinquentin.github.io/CarlinQuentin.github.io-api/'>Recipe Finder<img src={img4}/></a>  Built after 2 weeks of coding <br/> Time to build: 5 days </div>
-                        <div className='grid-item'><a target='blank' href='https://storylog1-client.herokuapp.com/'> Story Log<img src={img5}/></a>  Built after 8 weeks of coding <br/> Time to build: 2 weeks</div>
-                        <div className='grid-item'><a target='blank' href='https://food-finder123.herokuapp.com/'> Food Finder<img src={img6}/></a>  Built after 11 weeks of coding <br/> Time to build: 1 day</div>
-                        <div className='grid-item'><a target='blank' href='https://nightbuzz-plzzz-wurk.herokuapp.com/about'> Night Buzz<img src={img7}/></a>  Built after 10 weeks of coding <br/> Time to build: 2 weeks</div>
+                        <div className='grid-item'><a className='links' target='blank' href='https://codepen.io/QuentinCarlin/pen/aaNgEL'> CSS Creature<img src={img2}/></a>   Built after 1 day of coding <br/> Time to build: 1 day </div>
+                        <div className='grid-item'><a className='links' target='blank' href='https://carlinquentin.github.io/firstWebsite/'> Static Store Front<img src={img3}/></a>  Built after 5 days of coding <br/> Time to build: 3 days </div>
+                        <div className='grid-item'><a className='links' target='blank' href='https://carlinquentin.github.io/CarlinQuentin.github.io-api/'>Recipe Finder<img src={img4}/></a>  Built after 2 weeks of coding <br/> Time to build: 5 days </div>
+                        <div className='grid-item'><a className='links' target='blank' href='https://storylog1-client.herokuapp.com/'> Story Log<img src={img5}/></a>  Built after 8 weeks of coding <br/> Time to build: 2 weeks</div>
+                        <div className='grid-item'><a className='links' target='blank' href='https://food-finder123.herokuapp.com/'> Food Finder<img src={img6}/></a>  Built after 11 weeks of coding <br/> Time to build: 1 day</div>
+                        <div className='grid-item'><a className='links' target='blank' href='https://nightbuzz-plzzz-wurk.herokuapp.com/about'> Night Buzz<img src={img7}/></a>  Built after 10 weeks of coding <br/> Time to build: 2 weeks</div>
                     </div>
                 </div>
             )
